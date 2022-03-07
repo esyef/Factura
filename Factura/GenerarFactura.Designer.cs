7b0f24bad270c7a1f163b21c83c5d63c6842d1d1
YeffWeb <yeferson.dev@gmail.com>
2022-03-07 16:14:29 -0500

    Formulario Imprimir factura

---
+++ Factura/GenerarFactura.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Factura
+{
+    partial class GenerarFactura
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEstrato = new System.Windows.Forms.TextBox();
+            this.Consumo = new System.Windows.Forms.Label();
+            this.txtConsumo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBarrio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.cbSeleccionar = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbSeleccionar);
+            this.groupBox1.Controls.Add(this.txtDireccion);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtBarrio);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtConsumo);
+            this.groupBox1.Controls.Add(this.Consumo);
+            this.groupBox1.Controls.Add(this.txtEstrato);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(447, 140);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Estrato";
+            // 
+            // txtEstrato
+            // 
+            this.txtEstrato.Enabled = false;
+            this.txtEstrato.Location = new System.Drawing.Point(76, 72);
+            this.txtEstrato.Name = "txtEstrato";
+            this.txtEstrato.Size = new System.Drawing.Size(48, 23);
+            this.txtEstrato.TabIndex = 1;
+            // 
+            // Consumo
+            // 
+            this.Consumo.AutoSize = true;
+            this.Consumo.Location = new System.Drawing.Point(7, 104);
+            this.Consumo.Name = "Consumo";
+            this.Consumo.Size = new System.Drawing.Size(63, 16);
+            this.Consumo.TabIndex = 0;
+            this.Consumo.Text = "Estrato";
+            // 
+            // txtConsumo
+            // 
+            this.txtConsumo.Enabled = false;
+            this.txtConsumo.Location = new System.Drawing.Point(76, 101);
+            this.txtConsumo.Name = "txtConsumo";
+            this.txtConsumo.Size = new System.Drawing.Size(48, 23);
+            this.txtConsumo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Barrio";
+            // 
+            // txtBarrio
+            // 
+            this.txtBarrio.Enabled = false;
+            this.txtBarrio.Location = new System.Drawing.Point(251, 104);
+            this.txtBarrio.Name = "txtBarrio";
+            this.txtBarrio.Size = new System.Drawing.Size(176, 23);
+            this.txtBarrio.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(166, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Dirección";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Enabled = false;
+            this.txtDireccion.Location = new System.Drawing.Point(251, 74);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(176, 23);
+            this.txtDireccion.TabIndex = 1;
+            // 
+            // cbSeleccionar
+            // 
+            this.cbSeleccionar.FormattingEnabled = true;
+            this.cbSeleccionar.Location = new System.Drawing.Point(0, 23);
+            this.cbSeleccionar.Name = "cbSeleccionar";
+            this.cbSeleccionar.Size = new System.Drawing.Size(432, 24);
+            this.cbSeleccionar.TabIndex = 2;
+            // 
+            // GenerarFactura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 376);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "GenerarFactura";
+            this.Text = "Generar Facturas";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbSeleccionar;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBarrio;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtConsumo;
+        private System.Windows.Forms.Label Consumo;
+        private System.Windows.Forms.TextBox txtEstrato;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
--- Factura/GenerarFactura.cs
+++ Factura/GenerarFactura.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Factura
 {
-    public partial class Form1 : Form
+    public partial class GenerarFactura : Form
     {
-        public Form1()
+        public GenerarFactura()
         {
             InitializeComponent();
         }
